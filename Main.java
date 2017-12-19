1ad8160a9bc3f62aac978c1caea90cc1122a9549
Pradyuman <meetupradyuman@gmail.com>
2017-12-19 11:43:11 +0530

    Hello World Program added

---
+++ Main.java
@@ -0,0 +1,11 @@
+// This program prints Hello world
+// we have class named Main which is visible to all due to keyworld 'public'
+public class Main
+{
+	// Here we created main method which is static and return nothing
+	public static void main( String[] args)
+	{
+		// next line prints Hello World
+		System.out.println("Hello World");
+	}
+}
