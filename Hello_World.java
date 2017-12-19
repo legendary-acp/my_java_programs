37aa3cfd108867f6033fb087fdd6fade31ea831f
Pradyuman <meetupradyuman@gmail.com>
2017-12-19 11:53:55 +0530

    Constraints changed

---
+++ Hello_World.java
@@ -0,0 +1,11 @@
+// This program prints Hello world
+// we have class named Main which is visible to all due to keyworld 'public'
+public class Hello_World
+{
+	// Here we created main method which is static and return nothing
+	public static void main( String[] args)
+	{
+		// next line prints Hello World
+		System.out.println("Hello World");
+	}
+}
