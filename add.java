9d8266ef71c72575743fa3340c532914ea386bba
Pradyuman <meetupradyuman@gmail.com>
2017-12-19 11:49:50 +0530

    Adding numbers

---
+++ add.java
@@ -0,0 +1,13 @@
+public class add
+{
+	public static void main(String[] args)
+	{
+		int a,b;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter First Number:");
+		a= in.nextInt();
+		System.out.println("Enter Second Number:");
+		b= in.nextInt();
+		System.out.println("Their sum is %d", a+b);
+	}
+}
